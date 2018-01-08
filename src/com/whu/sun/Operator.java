27563a4bb538c90eb2bba8d74f1f319a8bc55a23
Marc <710641245@qq.com>
2018-01-08 20:14:12 +0800

    fix

--- src/com/whu/sun/Operator.java
+++ src/com/whu/sun/Operator.java
@@ -15,6 +15,12 @@ public class Operator {
 
         int res3 = operator.aminusb(1, 3);
         System.out.println(res3);
+
+        long res4 = operator.jiecheng(11);
+        System.out.println(res4);
+
+        long res5 = operator.trailingZeros(105);
+        System.out.println(res5);
     }
 
     //加法
@@ -47,5 +53,42 @@ public class Operator {
     //减法
     public int aminusb(int a, int b) {
         return aplusb_3(a, aplusb_3(~b, 1));
+    }
+
+    //阶乘
+    private long jiecheng(long n) {
+        if (n < 0) return -1;
+
+        if (n == 0) return 1;
+
+        return n * jiecheng(n - 1);
+    }
+
+    /*
+     * 找出n的阶乘的结果尾部0的个数
+     * @param n: An integer
+     * @return: An integer, denote the number of trailing zeros in n!
+     */
+    public long trailingZeros(long n) {
+        // write your code here, try to do it without arithmetic operators.
+
+//        long res = jiecheng(n);
+//
+//        int count = 0;
+//        while (res % 10 == 0) {
+//            res = res / 10;
+//            count++;
+//        }
+//
+//        return count;
+
+        long count = 0;
+        long temp = n / 5;
+        while (temp != 0) {
+            count += temp;
+            temp /= 5;
+        }
+
+        return count;
     }
 }
